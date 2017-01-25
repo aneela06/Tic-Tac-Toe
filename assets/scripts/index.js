af8c8a2ab03d23e9ecc959a692615988536e717a
aneela06 <ahmedaneela@gmail.com>
2017-01-24 22:03:24 -0500

    Add gameboard, event listener, game logic
    index.html: divs added with data attributes
    index.js: eventListener, piecePlayed function added

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,32 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+// $(".cell").forEach(addEventListener("click",printer);
+let turn = "player_x";
+let board = ['','','','','','','','',''];
+
+let cells = document.querySelectorAll('.cell');
+
+let placePiece = function(index,turn){
+  if (board[index] === ''){
+    if (turn === 'player_x'){
+      board[index] = 'x';
+      turn = 'player_o';
+      return turn;
+    } else {
+      board[index] = 'o';
+      turn = 'player_x';
+      return turn;
+    }
+  }
+  return turn;
+};
+
+function gameLogic(){
+  let index = $(this).data('name');
+  turn = placePiece(index,turn);
+  console.log(board);
+}
+
+cells.forEach(cell => cell.addEventListener('click',gameLogic));
