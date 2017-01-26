5cdd900408f385d8390299c4ba61c650209cc821
aneela06 <ahmedaneela@gmail.com>
2017-01-25 20:57:11 -0500

    Check for win
    Added function to check for conditions for winning, reset game if won

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const config = {
+  apiOrigins: {
+    production: 'http://aqueous-atoll-85096.herokuapp.com',
+    development: 'http://tic-tac-toe.wdibos.com',
+  },
+};
+
+// require('../config');
+
+const app = require('../app');
+
+// const getFormFields = require('../../../lib/get-form-fields.js');
+
+//authApi.signUp(authUi.success, authUi.failure, data);
+
+const signUp = function (data) {
+  console.log(data);
+  return $.ajax({
+    url: config.apiOrigins + '/sign-up/',
+    method: 'POST',
+    data,
+  });
+};
+
+const signIn = function (data) {
+  console.log(data);
+  return $.ajax({
+    url: config.apiOrigins + '/sign-in/',
+    method: 'POST',
+    data,
+  });
+};
+
+const signOut = function () {
+  return $.ajax({
+    method: 'DELETE',
+    url: config.apiOrigins + '/sign-out/' + app.user.id,
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+  });
+};
+
+const changePassword = function (data) {
+  return $.ajax({
+    method: 'PATCH',
+    url: config.apiOrigins + '/change-password/' + app.user.id,
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+    data: data,
+  });
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+};
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -18,23 +18,108 @@ let turn = 'player_x';
 
 // Variable board - holds x's and o's
 let board = ['', '', '', '', '', '', '', '', ''];
+let gameOver = false;
 
 // Selects all cell Divs from HTML
 let cells = document.querySelectorAll('.cell');
+
+let checkO = function(element) {
+  if (element === 'o') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+let checkX = function(element) {
+  if (element === 'x') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+let isWinner = function (board,turn) {
+  let condition1 = [board[0],board[1],board[2]];
+  let condition2 = [board[3],board[4],board[5]];
+  let condition3 = [board[6],board[7],board[8]];
+  let condition4 = [board[0],board[3],board[6]];
+  let condition5 = [board[1],board[4],board[7]];
+  let condition6 = [board[2],board[5],board[8]];
+  let condition7 = [board[2],board[4],board[6]];
+  let condition8 = [board[0],board[4],board[8]];
+  let myFunc;
+
+  if (turn === 'player_x') {
+    myFunc = checkX;
+  } else {
+    myFunc = checkO;
+  }
+
+  if (condition1.every(myFunc) === true) {
+    return true;
+  }
+  if (condition2.every(myFunc) === true) {
+    return true;
+  }
+  if (condition3.every(myFunc) === true) {
+    return true;
+  }
+  if (condition4.every(myFunc) === true) {
+    return true;
+  }
+  if (condition5.every(myFunc) === true) {
+    return true;
+  }
+  if (condition6.every(myFunc) === true) {
+    return true;
+  }
+  if (condition7.every(myFunc) === true) {
+    return true;
+  }
+  if (condition8.every(myFunc) === true) {
+    return true;
+  }
+  return false;
+};
+
+// let cellFilled = function (element) {
+//   if (element !== '') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// board.every(cellFilled)
+let resetBoard = function () {
+  turn = 'player_x';
+  board = ['', '', '', '', '', '', '', '',''];
+};
 
 let placePiece = function (index, turn) {
   if (board[index] === '') {
     if (turn === 'player_x') {
       board[index] = 'x';
+      gameOver = isWinner(board,turn);
+      if (gameOver === true) {
+        alert("You WONNNN!!!!!");
+        resetBoard();
+        return turn;
+      }
       turn = 'player_o';
       return turn;
     } else {
       board[index] = 'o';
+      gameOver = isWinner(board,turn);
+      if (gameOver === true) {
+        alert("You WONNNN!!!!!");
+        resetBoard();
+        return turn;
+      }
       turn = 'player_x';
       return turn;
     }
   }
-
   return turn;
 };
 
